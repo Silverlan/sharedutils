86cd09a8a554d9623ca0e28330a6d34e93f7c2ea
Silverlan <silverlann@gmail.com>
2020-12-17 12:56:35 +0100

    Add util::shutdown_os

--- src/util.cpp
+++ src/util.cpp
@@ -21,6 +21,8 @@
 	#include <pthread>
 	#include <stdio.h>
 	#include <dlfcn.h>
+	#include <unistd.h>
+	#include <linux/reboot.h>
 #else
 	#include "Shlwapi.h"
 	#include <vector>
@@ -828,5 +830,47 @@ void util::flash_window()
 	fi.uCount = 0;
 	fi.dwTimeout = 0;
 	FlashWindowEx(&fi);
+#endif
+}
+
+bool util::shutdown_os()
+{
+#ifdef _WIN32
+	// See https://docs.microsoft.com/en-us/windows/win32/shutdown/how-to-shut-down-the-system?redirectedfrom=MSDN
+	HANDLE hToken; 
+	TOKEN_PRIVILEGES tkp; 
+ 
+	// Get a token for this process. 
+	if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hToken)) 
+		return( FALSE ); 
+ 
+	// Get the LUID for the shutdown privilege. 
+ 
+	LookupPrivilegeValue(NULL, SE_SHUTDOWN_NAME, 
+	&tkp.Privileges[0].Luid); 
+ 
+	tkp.PrivilegeCount = 1;  // one privilege to set    
+	tkp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED; 
+ 
+	// Get the shutdown privilege for this process. 
+ 
+	AdjustTokenPrivileges(hToken, FALSE, &tkp, 0, (PTOKEN_PRIVILEGES)NULL, 0); 
+ 
+	if (GetLastError() != ERROR_SUCCESS) 
+		return FALSE; 
+ 
+	// Shut down the system and force all applications to close. 
+ 
+	if (!ExitWindowsEx(EWX_SHUTDOWN | EWX_FORCE, 
+		SHTDN_REASON_MAJOR_OPERATINGSYSTEM |
+		SHTDN_REASON_MINOR_UPGRADE |
+		SHTDN_REASON_FLAG_PLANNED)) 
+		return FALSE; 
+
+	//shutdown was successful
+	return TRUE;
+#else
+	reboot(LINUX_REBOOT_MAGIC1,LINUX_REBOOT_MAGIC2,LINUX_REBOOT_CMD_POWER_OFF,0);
+	return true;
 #endif
 }
