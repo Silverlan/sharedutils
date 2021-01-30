d5a1a8185d30a2ad693b5e015cf16c8e339b8926
Silverlan <silverlann@gmail.com>
2021-01-30 11:11:48 +0100

    Add ufile::remove_extension_from_filename overload with list of extensions to consider

--- include/sharedutils/util_file.h
+++ include/sharedutils/util_file.h
@@ -18,6 +18,18 @@ namespace ufile
 	DLLSHUTIL void remove_extension_from_filename(std::string &str);
 	DLLSHUTIL std::vector<std::string> split_path(const std::string &path);
 	DLLSHUTIL std::string to_path(const std::vector<std::string> &splitPath,size_t startPos=0ull,size_t endPos=std::string::npos);
+	template<typename TList>
+		void remove_extension_from_filename(std::string &str,const TList &extensions)
+	{
+		std::string ext;
+		if(get_extension(str,&ext) == false)
+			return;
+		ustring::to_lower(ext);
+		auto it = std::find(extensions.begin(),extensions.end(),ext);
+		if(it == extensions.end())
+			return;
+		str = str.substr(0,str.length() -(ext.length() +1));
+	}
 }
 
 #endif
