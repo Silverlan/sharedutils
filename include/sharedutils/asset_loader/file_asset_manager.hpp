c090cefbad5889f9df8317b6eced695d014eeaf3
Silverlan <silverlann@gmail.com>
2022-07-16 19:54:09 +0200

    Make FileAssetManager::ClearCachedResult protected

--- include/sharedutils/asset_loader/file_asset_manager.hpp
+++ include/sharedutils/asset_loader/file_asset_manager.hpp
@@ -100,6 +100,7 @@ namespace util
 		virtual void Poll();
 	protected:
 		virtual void Reset() override;
+		void ClearCachedResult(size_t hash);
 		virtual void InitializeProcessor(util::IAssetProcessor &processor)=0;
 		virtual util::AssetObject InitializeAsset(const Asset &asset,const util::AssetLoadJob &job)=0;
 		void OnAssetReloaded(const std::string &assetName);
@@ -154,7 +155,6 @@ namespace util
 		util::FileAssetManager::PreloadResult::Result CacheResult(size_t hash,util::FileAssetManager::PreloadResult::Result result);
 		util::FileAssetManager::PreloadResult::Result CacheResult(const std::string &assetName,util::FileAssetManager::PreloadResult::Result result);
 		std::optional<util::FileAssetManager::PreloadResult> GetCachedResult(size_t hash) const;
-		void ClearCachedResult(size_t hash);
 		std::function<std::optional<std::string>(const std::string&,const std::string&)> m_externalSourceFileImportHandler = nullptr;
 		std::unordered_map<size_t,std::vector<std::function<void(util::Asset*,AssetLoadResult)>>> m_callOnLoad;
 		std::unique_ptr<AssetFileHandler> m_fileHandler;
